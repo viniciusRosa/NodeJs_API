efa3ffdd1e0712b3fed58ae8b8d1bcc6a3ed991d
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-01-23 17:06:29 -0300

    forgot password already

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -3,7 +3,8 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../../config/auth')
-
+const crypto = require('crypto');
+const mailer = require('../../modules/mailer');
 
 const router = express.Router();
 
@@ -52,6 +53,85 @@ router.post('/authenticate', async (req, res) => {
             user, 
             token: generateToken({id: user._id}) 
         });
+
+})
+
+
+router.post('/forgot_password', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+
+        const user = await User.findOne({ email });
+
+        if(!user) {
+            return res.status(400).send({ error: 'User not found' });
+        }
+
+        const token = crypto.randomBytes(20).toString('hex');
+
+        const now = new Date();
+        now.setHours(now.getHours() + 1);
+
+        await User.findByIdAndUpdate(user._id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordResetExpires: now,
+            }
+        })
+
+      
+      mailer.sendMail({
+          to: email,
+          from: 'vinicius.rosa00@gmail.com',
+          template: '/forgot_password', 
+          context: { token },
+      }, (err) => {
+          if(err) {
+              console.log(err)
+              return res.status(400).send({ error: 'cannot send forgot password email' });
+          }
+
+          return res.send()
+      })  
+
+    }catch(err) {
+        console.log(err);
+        res.status(400).send({ error: 'Error on forgot password, try again' })
+    }
+})
+
+router.post('/reset_password', async (req, res) => {
+    const { email, token, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email })
+        .select('+passwordResetToken passwordResetExpire');
+
+        if(!user) {
+            return res.status(400).send({ error: 'User not found' });
+        }
+
+        if(token !== user.passwordResetToken) {
+            return res.status(400).send({ error: "token invalid" })
+        }
+
+        const now = new Date();
+
+        if(now > user.passwordResetExpires) {
+            return res.status(400).send({ error: "token expired, generate a new one" })
+        }
+
+        user.password = password;
+
+        await user.save();
+
+        res.send();
+
+
+    }catch(err){
+        res.status(400).send({ error: "cannot reset yoru password, try again" })
+    }
 
 })
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-require('./app/controllers/authController')(app);
-require('./app/controllers/projectController')(app);
-
+require('./app/controllers/index')(app); 
 
 app.listen(2323, () => {
     console.log('ok');
